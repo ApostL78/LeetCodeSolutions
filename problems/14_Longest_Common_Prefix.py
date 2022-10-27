a10308affaa089b2190e43309f8b9cff0008a844
ApostL78 <vgwrbrvw@mail.ru>
2022-10-27 21:22:16 +0400

    [add] 14_Longest_Common_Prefix.py
    Signed-off-by: ApostL78 <vgwrbrvw@mail.ru>

---
+++ problems/14_Longest_Common_Prefix.py
@@ -0,0 +1,35 @@
+from typing import List
+import pytest
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+
+        for j in range(len(strs[0])):
+            for i in range(len(strs) - 1):
+                if len(strs[i + 1]) < j + 1:
+                    return res
+                if strs[i][j] == strs[i + 1][j]:
+                    continue
+                else:
+                    return res
+            res += strs[0][j]
+        return res
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    (["flower", "flow", "flight"], "fl"),
+    (["dog", "racecar", "car"], ""),
+    ([""], ""),
+    (["dog"], "dog"),
+    (["veryLongTextveryLongTextveryLongTextveryLongTxtveryLongText", ""], ""),
+    (["objectifying", "objectifying", "objectifying"], "objectifying"),
+    (["dog", "racecar", "car", ""], ""),
+    (["just", "justification", "justify"], "just"),
+    (["dog", "door", "doom"], "do"),
+    (["iterator", "italic", "item"], "it")
+])
+def test_contains_duplicate(test_input, expected):
+    solution = Solution()
+    assert solution.longestCommonPrefix(test_input) == expected
